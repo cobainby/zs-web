c92e89ff4478db1470c1d1d5f55dad75877012ce
baiyang <619148077@qq.com>
2018-09-13 16:52:08 +0800

    改变api后端交互相对路径

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -31,7 +31,7 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
-  },
+  }, 
   externals: {
     'AMap': 'AMap',
     'AMapUI': 'AMapUI'
