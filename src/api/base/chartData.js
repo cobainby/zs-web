89d4280ff9ea628763a909444f4f19f8bc8e2045
coderbaiyang <619148077@qq.com>
2019-08-22 11:29:11 +0800

    新日报

--- src/api/base/chartData.js
+++ src/api/base/chartData.js
@@ -10,7 +10,7 @@ export const getQx = data =>createAPI(`/fdData/qx`);//倾斜数据
 export const getLf = data =>createAPI(`/fdData/Lf`);//裂缝数据
 export const getMonitorTotal=data=>createAPI(`/monitorTotal/gets`, "get", data);//拿到所有监测项数据统计
 export const verticalExport = data => createAPI("/fdData/vertical/export", "get", data);//获得竖向位移报表导出接口
-export const dailyExport = data=> createAPI("/daily/export","post",data);//日报导出功能
+export const dailyExport = data=> createAPI("/Export/daily","post",data);//日报导出功能
 export const fdPatrolList=data=>createAPI("/fdPatrol/history/get","get",data);//拿到所有巡视监测数据列表
 export const fdPatrol=data=>createAPI(`/fdPatrol/history/${data.uuid}`,"get",data);//拿到所选巡视记录详情
 export const dailyList=data=>createAPI("/daily/logList/get","get",data);//拿到日报记录详情
